5cc6d8eb3746af324ef19249c6fef91855346c4a
SheikhAlUsama <82088182+SheikhAlUsama@users.noreply.github.com>
2021-05-27 02:07:01 +0500

    assignment 8 javascript 26 to 30

---
+++ app.js
@@ -0,0 +1,61 @@
+    //   Q 1
+    // var num = + prompt("Enter any no.")
+    // document.write("number" + " " + num + "<br/>")
+    //  var num2 = Math.round(num)
+    // document.write("Round of value" + " " + num2 + "<br/>")
+    // var num3 = Math.floor(num)
+    // document.write("floor of value" + " " + num3 + "<br/>")
+    // var num4 = Math.ceil(num)
+    // document.write("ceil of value" + " " + num4 + "<br/>")
+
+        // Q   2
+    // var num = + prompt("Enter any nagetive no.")
+    // document.write("number" + " " + num + "<br/>")
+    //  var num2 = Math.round(num)
+    // document.write("Round of value" + " " + num2 + "<br/>")
+    // var num3 = Math.floor(num)
+    // document.write("floor of value" + " " + num3 + "<br/>")
+    // var num4 = Math.ceil(num)
+    // document.write("ceil of value" + " " + num4 + "<br/>")
+
+        //  Q   3
+    // var input = + prompt("Enter value")
+    // value = Math.abs(input);
+    // console.log(value); 
+    
+
+
+        //   Q  4
+
+
+        // var randomNumber = Math.ceil (Math.random() * 5) +1;
+        // document.write("Your rolled number is " + randomNumber )
+
+            //   Q  5
+            // var head = 1;
+            // var tail = 2;
+            
+            // var toss = Math.random() * 2;
+            // var floor = Math.floor(toss)
+            // if(floor === 0){
+            //     document.write("0 <br> Random Coin Value: Head")
+            // } else if(floor === 1)
+            // {
+            //     document.write("1 <br> Random Coin Value: Tails")
+            // }
+        // Q    6
+        // var random = Math.round(Math.random()* 100)
+        // console.log(random)
+
+    // Q   8
+// var userInput = + prompt("Enter nuber between 1 t 10")
+// var random = Math.round(Math.random()*10)
+// if(random === userInput){
+
+//     alert("You got the secret number")
+// }
+// else{
+//     alert ("Try Again")
+// }
+
+
